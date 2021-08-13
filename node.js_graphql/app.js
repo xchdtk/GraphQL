d4dbf74b80eaa4785ff33e6a1a1ad58141416c64
kimjinsoo <1004kjs_@naver.com>
2021-08-13 13:04:47 +0900

    grathql mutation(add, update, delete) 학습

--- node.js_graphql/app.js
+++ node.js_graphql/app.js
@@ -5,35 +5,67 @@ const port = 8080;
 const app = express();
 
 const schema = buildSchema(`
-    type Query {
-        pizzabuns : [PizzaBun]
-        hello : String
+    input ProductInput {
+        name: String
+        price: Int
+        description: String
     }
 
-    type PizzaBun {
-        pb_idx: ID,
-        pb_name: String,
-        size: Int,
-        bread: String,
-        sauce: String,
-        topping: String
+    type Query {
+        getProduct(id:ID!) : Product
     }
+
+    type Mutation {
+        addProduct( input : ProductInput) : Product
+        updateProduct( id: ID!, input: ProductInput!): Product
+        deleteProduct( id: ID!): Product
+    }
+
+    type Product {
+        id: ID!
+        name: String
+        price: Int
+        description: String
+    }    
 `)
 
-const resolver = {
-    pizzabuns: () => {
-        return [{
-                "pb_idx"  : "1",
-                "pb_name" : "sausage",
-                "size"    : 6,
-                "bread" : "wheat",
-                "sauce" : "tomato",
-                "topping" : "sausage"
-            }
-        ]
+const products = [
+    {
+        id: 1,
+        name: "맜있어",
+        price: 2000,
+        description: '맜있어 정말'
     },
-    hello: () => {
-        return "hello love"
+    {
+        id: 2,
+        name: "행복해",
+        price: 4000,
+        description: '행복해 정말'
+    },
+]
+
+const root = {
+    getProduct : ({id}) => products.find( product => product.id === parseInt(id)),
+    addProduct : ({input})  => {
+        input.id = parseInt(products.length+1);
+        products.push(input);
+        return root.getProduct({ id: input.id})
+    },
+
+    updateProduct : ({id, input}) => {
+        const index = products.findIndex(product => product.id === parseInt(id))
+        products[index] = {
+            id: parseInt(id),
+            ...input
+        }
+        return products[index]
+    },
+
+    deleteProduct : ({id}) => {
+        const index = products.findIndex( product => product.id === parseInt(id))
+        const product = products.splice(index, 1);
+        console.log(product)
+        return product[0]
     }
 }
 
@@ -41,7 +73,7 @@ const resolver = {
 
 app.use("/graphql", graphqlHTTP({
     schema: schema,
-    rootValue: resolver,
+    rootValue: root,
     graphiql: true
 }))
 
