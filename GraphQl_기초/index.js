2163de30ded0ed98e0c5961e0c4cf560fab5b02b
kimjinsoo <1004kjs_@naver.com>
2021-09-25 10:49:43 +0900

    이전 커밋 삭제

--- GraphQl_기초/index.js
+++ GraphQl_기초/index.js
@@ -3,10 +3,29 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    team(id:Int) : Team
     equipments: [Equipment]
-    supplies: [Supply]
-    team(id: Int): Team
+    supplies: [Supplies]
   }
+
+  type Mutation {
+    deleteEquipment(id: String): Equipment
+
+    insertEquipment(
+      id: String,
+      used_by: String,
+      count: Int,
+      new_or_used: String
+    ): Equipment
+
+    editEquipment(
+      id: String,
+      used_by: String,
+      count: Int,
+      new_or_used: String
+    ):Equipment
+  }
+
   type Team {
     id: Int
     manager: String
@@ -15,40 +34,66 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
-    supplies: [Supply]
+    supplies: [Supplies]
   }
   type Equipment {
-    id: String
-    used_by: String
-    count: Int
-    new_or_used: String
+      id: String
+      used_by: String
+      count: Int
+      new_or_used: String
   }
-
-  type Supply {
-    id: String
-    team: Int
+  type Supplies {
+      id: String
+      team: Int 
   }
 `
 const resolvers = {
-  // Query는 데이터 조회
   Query: {
+    // ...
     teams: () => database.teams
     .map((team) => {
-      team.supplies = database.supplies
-      .filter((supply) => {
-        return supply.team == team.id
-      })
-      return team
+        team.supplies = database.supplies
+        .filter((supply) => {
+            return supply.team === team.id
+        })
+        return team
     }),
-    // filter는 배열을 반환하기에 [0]을 붙여주어야 한다
-    team: (parent, args, context, info) => database.teams
-          .filter((team) => {
-            return team.id === args.id
-          })[0],
+    team: (parent, args, context, info) => database.teams.filter((team) => {
+        return team.id === args.id
+    })[0],
     equipments: () => database.equipments,
-    supplies: () => database.supplies,
-  }
+    supplies: () => database.supplies
+  },
+
+  Mutation: {
+      deleteEquipment: (parent, args, context, info) => {
+          const deleted = database.equipments
+              .filter((equipment) => {
+                  return equipment.id === args.id
+              })[0]
+          database.equipments = database.equipments
+              .filter((equipment) => {
+                  return equipment.id !== args.id
+              })
+          return deleted
+      },
+
+      insertEquipment: (parent, args, context, info) => {   
+          database.equipments.push(args)
+          return args
+      },
+
+      editEquipment: (parent, args, context, info) => {
+        return database.equipments.filter((equipment) => {
+            return equipment.id === args.id
+        }).map((equipment) => {
+            Object.assign(equipment, args)
+            return equipment
+        })[0]
+    },
+    // ...
 }
+  }
 const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({ url }) => {
 console.log(`🚀  Server ready at ${url}`)
